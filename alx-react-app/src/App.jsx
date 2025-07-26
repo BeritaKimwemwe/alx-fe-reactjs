dee5d50aa3bc475cb1e88cdd3f7b4a49805cb893
Berita Kimwemwe <bkimwemweh@gmail.com>
2025-07-26 19:55:37 +0300

    Impliment Header,MainContent,Footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,12 @@
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-import WelcomeMessage from './components/WelcomeMessage'; // ✅ Correct path
 
 function App() {
   return (
     <>
       <Header />
       <MainContent />
-      <WelcomeMessage />
       <Footer />
     </>
   );
