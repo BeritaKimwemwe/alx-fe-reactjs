9dbdd7d091e3c9476b57a5af8875d9c677d0e3a3
Berita Kimwemwe <bkimwemweh@gmail.com>
2025-07-30 13:25:31 +0300

    Updated API

--- github-user-search/src/services/github.js
+++ github-user-search/src/services/github.js
@@ -1,3 +1,16 @@
 // src/services/github.js
+import axios from 'axios';
+
 const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
-// Add axios setup here later
+
+// Function to fetch user data from GitHub
+const fetchUserData = async (username) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`, {
+    headers: {
+      Authorization: GITHUB_API_KEY ? `token ${GITHUB_API_KEY}` : undefined,
+    },
+  });
+  return response.data;
+};
+
+export default fetchUserData;
