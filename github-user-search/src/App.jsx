b394253dddcd2eeb1423ddac1ca5a3da621e6557
Berita Kimwemwe <bkimwemweh@gmail.com>
2025-07-30 13:07:10 +0300

    Add GitHub User Search React project setup

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      {/* SearchBar and Results will go here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ github-user-search/src/services/github.js
@@ -0,0 +1,3 @@
+// src/services/github.js
+const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
+// Add axios setup here later
