15b53cddf3407829ab32a7867fdcc2e0abe12d35
Berita Kimwemwe <bkimwemweh@gmail.com>
2025-07-30 13:34:45 +0300

    File name change to githubServices.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,9 +1,9 @@
-// src/services/github.js
+
 import axios from 'axios';
 
 const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
-// Function to fetch user data from GitHub
+
 const fetchUserData = async (username) => {
   const response = await axios.get(`https://api.github.com/users/${username}`, {
     headers: {
