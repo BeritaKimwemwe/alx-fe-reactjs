6300bbe4477b45c2b5c5fd5446cf004f44e27c51
Berita Kimwemwe <bkimwemweh@gmail.com>
2025-08-13 22:02:19 +0300

    Commit changes made

--- alx-react-app-new/alx-react-app-new/src/App.jsx
+++ alx-react-app-new/alx-react-app-new/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
-import UserProfile from "./components/UserProfile";
 import MainContent from "./components/MainContent";
+import UserProfile from "./components/UserProfile";
 import Footer from "./components/Footer";
 
 const App = () => {
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 const MainContent = () => {
   return (
     <main style={{ padding: '10px', backgroundColor: '#f4f4f4' }}>
-      <p>Welcome to the user profile app.</p>
+      <p>I love to visit New York, Paris , and Tokyo.</p>
     </main>
   );
 };
