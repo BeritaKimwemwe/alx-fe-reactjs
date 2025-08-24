474a94ecffb73b990578c049c6632114acd9e4e0
Berita Kimwemwe <bkimwemweh@gmail.com>
2025-08-24 22:58:12 +0300

    Added React Query Demo project with PostsComponent inside components folder

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import PostsComponent from './PostsComponent';
+import PostsComponent from './components/PostsComponent';
 
 // Create a client instance
 const queryClient = new QueryClient();
